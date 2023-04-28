90118a5af1c01ede6525dde23735a8cf102a27b8
SJasonHumphrey <sjasonhumphrey@gmail.com>
2023-04-28 00:22:32 -0500

    added state to list items

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import "./App.css";
 import ListGroup from "./components/ListGroup";
-import Message from "./components/ListGroup";
 
 function App() {
-  return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
-  );
+  return <ListGroup/>
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,30 @@
+import { useState } from "react";
+
+
 function ListGroup() {
-    return <h1>List Group</h1>
-}
+
+    let items = ["New York", "Nashville"];
+    
+
+    const [selectedIndex, setSelectedIndex] = useState (-1)
+
+    return (
+      <>
+        <h1>List</h1>
+        {items.length === 0 && <p>Items Not found</p>}
+        <ul className="list-group">
+          {items.map((item, index) => (
+            <li key={index} 
+                onClick={() => {
+                  setSelectedIndex(index)
+                }}
+                className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 
 export default ListGroup
